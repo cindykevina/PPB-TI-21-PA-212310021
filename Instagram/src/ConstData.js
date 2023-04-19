c5dc2ae290c52c0c802b4c5e027a06946676bf19
Cindy Kevina <113493253+cindykevina@users.noreply.github.com>
2023-04-19 23:46:24 +0700

    Add files via upload

---
+++ Instagram/src/ConstData.js
@@ -0,0 +1,14 @@
+const UserObj = [
+    { id: 1, name: "Kevina", username: "Cindykevinaaaa", img: require("../assets/images/foto.jpg"), gender: "F", is_view: true, cap: "Kedipin dulu"},
+    { id: 2, name: "Melanie", username: "Mvgrc", img: require("../assets/images/meng.jpeg"), gender: "F", is_view: true, cap: "Main main main" },
+    { id: 3, name: "Chamber", username: "ThatFrenchGuy", img: require("../assets/images/cham.png"), gender: "M", is_view: true, cap: "Lamaran"},
+    { id: 4, name: "Phoenix", username: "TheBoss", img: require("../assets/images/pho.png"), gender: "M", is_view: true, cap: "Lemme show you how the boss does it" },
+    { id: 5, name: "Devin", username: "Pindiepiee", img: require("../assets/images/pin.jpg"), gender: "M", is_view: true, cap: "Ayang" },
+    { id: 6, name: "Frila", username: "Frilacahyawrdn", gender: "F", is_view: true },
+    { id: 7, name: "Teddy", username: "Michaelteddyyyy", gender: "M", is_view: false },
+    { id: 8, name: "Hera", username: "Hrdwiprdta_", gender: "F", is_view: false },
+    { id: 9, name: "Osa", username: "Rosalinepramesti", gender: "F", is_view: false },
+    { id: 10, name: "Daniel", username: "Danielsaputraaaa", gender: "M", is_view: false }
+];
+
+export { UserObj };
